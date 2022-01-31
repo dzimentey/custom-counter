f6c086404d520306ed81b439a46bbb3a83c9b0e9
dzimentey <dzimentey@gmail.com>
2022-01-31 15:04:11 +0300

    refactor: remove unused code

--- src/Components/ButtonComponent.tsx
+++ src/Components/ButtonComponent.tsx
@@ -6,17 +6,12 @@ type ButtonType = {
     maxValue?: number
     disable?: boolean
     click: () => void
-    //setLocal?: () => void
 }
 
 export const ButtonComponent = (props: ButtonType) => {
     return(
         <Button variant={"contained"} color={"primary"} size={"small"}
                 onClick = {props.click}
-                // onClick={()=>{
-                //     props.click();
-                //     props.setLocal && props.setLocal()
-                // }}
                 disabled={props.disable}>{props.title}
         </Button>
     )
